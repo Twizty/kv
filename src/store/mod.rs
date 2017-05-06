79f3b77d9fe174c24da31c5e30ec72d451c539a6
Maxim Davydov <maximdavydoff@yandex.ru>
2017-05-06 21:45:46 +0300

    Added expiration

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
-use std::sync::{Mutex};
+// use std::borrow::Borrow;
+use std::collections::hash_map::Entry;
+use std::sync::{Arc, Mutex};
 use std::borrow::Borrow;
+use std::time::{Instant};
 
 enum V {
   StringValue(String),
@@ -11,15 +14,29 @@ enum V {
 pub struct Store {
   mutex: Mutex<()>,
   store: HashMap<String, V>,
+  ttls:  HashMap<String, Instant>,
 }
 
 impl Store {
   pub fn new() -> Store {
-    Store { mutex: Mutex::new(()), store: HashMap::new() }
+    Store {
+      mutex: Mutex::new(()),
+      store: HashMap::new(),
+      ttls:  HashMap::new(),
+    }
   }
 
-  pub fn get(&self, key: String) -> Option<&String> {
+  pub fn get(&mut self, key: String) -> Option<&String> {
     let _data = self.mutex.lock().unwrap();
+    let now   = Instant::now();
+
+    if let Entry::Occupied(occ_e) = self.ttls.entry(key.clone()) {
+      if now >= *occ_e.get() {
+        occ_e.remove();
+        self.store.remove(&key);
+        return None
+      }
+    }
 
     match self.store.get(&key) {
       Some(value) => {
@@ -35,13 +52,21 @@ impl Store {
   pub fn set(&mut self, key: String, val: String) {
     let _data = self.mutex.lock().unwrap();
 
-    self.store.insert(key, V::StringValue(val));
+    self.store.insert(key.clone(), V::StringValue(val));
+  }
+
+  pub fn expire(&mut self, key: String, at: Instant) {
+    let _data = self.mutex.lock().unwrap();
+
+   self.ttls.insert(key.clone(), at); 
   }
 }
 
 
 mod test {
   use super::*;
+  use std::thread::sleep;
+  use std::time::{Duration};
 
   #[test]
   fn test_set_and_get() {
@@ -52,5 +77,18 @@ mod test {
 
     assert_eq!(s.get(key.clone().to_string()), Some(&(value.to_string())));
     assert_eq!(s.get("baz".to_string()), None);
+  }
+
+  fn test_get_after_expire() {
+    let mut s = Store::new();
+    let key = "foo";
+    let value = "bar";
+    s.set(key.clone().to_string(), value.clone().to_string());
+    s.expire(key.clone().to_string(), Instant::now() + Duration::new(1, 0));
+    assert_eq!(s.get(key.clone().to_string()), Some(&(value.to_string())));
+
+    sleep(Duration::new(1, 0));
+
+    assert_eq!(s.get(key.clone().to_string()), None);
   }
 }
