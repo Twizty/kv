82c7478ea1a42dda0c6513f2e20079fe6e2c94f5
Maxim Davydov <maximdavydoff@yandex.ru>
2017-05-07 15:53:18 +0300

    Added new actions for lists

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,8 +1,6 @@
 use std::collections::HashMap;
-// use std::borrow::Borrow;
 use std::collections::hash_map::Entry;
-use std::sync::{Arc, Mutex};
-use std::borrow::Borrow;
+use std::sync::{Mutex};
 use std::time::{Instant};
 
 const TYPE_MISSMATCH_ERROR: &'static str = "Type missmatch for the key";
@@ -19,6 +17,20 @@ pub struct Store {
   ttls:  HashMap<String, Instant>,
 }
 
+fn drop_if_expired(e: Entry<String, Instant>, store: &mut HashMap<String, V>) -> Option<()> {
+  let now = Instant::now();
+
+  if let Entry::Occupied(occ_e) = e {
+    if now >= *occ_e.get() {
+      store.remove(occ_e.key());
+      occ_e.remove();
+      return Some(())
+    }
+  }
+
+  None
+}
+
 impl Store {
   pub fn new() -> Store {
     Store {
@@ -30,14 +42,9 @@ impl Store {
 
   pub fn get(&mut self, key: String) -> Option<&String> {
     let _data = self.mutex.lock().unwrap();
-    let now   = Instant::now();
 
-    if let Entry::Occupied(occ_e) = self.ttls.entry(key.clone()) {
-      if now >= *occ_e.get() {
-        occ_e.remove();
-        self.store.remove(&key);
-        return None
-      }
+    if let Some(_) = drop_if_expired(self.ttls.entry(key.clone()), &mut self.store) {
+      return None
     }
 
     match self.store.get(&key) {
@@ -66,6 +73,8 @@ impl Store {
   pub fn l_append(&mut self, key: String, val: String) -> Result<(), &'static str> {
     let _data = self.mutex.lock().unwrap();
 
+    drop_if_expired(self.ttls.entry(key.clone()), &mut self.store);
+
     let mut value = self.store.entry(key.clone()).or_insert(V::ListValue(Vec::new()));
 
     if let &mut V::ListValue(ref mut s) = value {
@@ -73,6 +82,42 @@ impl Store {
       Ok(())
     } else {
       Err(TYPE_MISSMATCH_ERROR)
+    }
+  }
+
+  pub fn l_get(&mut self, key: String, index: &usize) -> Option<&String> {
+    let _data = self.mutex.lock().unwrap();
+
+    if let Some(_) = drop_if_expired(self.ttls.entry(key.clone()), &mut self.store) {
+      return None
+    }
+
+    match self.store.get(&key) {
+      Some(value) => {
+        match value {
+          &V::ListValue(ref v) if v.len() > *index => Some(&v[*index]),
+          _ => None,
+        }
+      },
+      None => None,
+    }
+  }
+
+  pub fn l_getall(&mut self, key: String) -> Option<&Vec<String>> {
+    let _data = self.mutex.lock().unwrap();
+
+    if let Some(_) = drop_if_expired(self.ttls.entry(key.clone()), &mut self.store) {
+      return None
+    }
+
+    match self.store.get(&key) {
+      Some(value) => {
+        match value {
+          &V::ListValue(ref v) => Some(v),
+          _ => None,
+        }
+      },
+      None => None,
     }
   }
 }
@@ -116,6 +161,8 @@ mod test {
     let result = s.l_append(key.clone().to_string(), value.clone().to_string());
 
     assert_eq!(result, Ok(()));
+    assert_eq!(s.l_get(key.clone().to_string(), &0), Some(&(value.to_string())));
+    assert_eq!(s.l_get(key.clone().to_string(), &1), None);
   }
 
   #[test]
@@ -128,5 +175,38 @@ mod test {
     let result = s.l_append(key.clone().to_string(), value.clone().to_string());
 
     assert_eq!(result, Err(TYPE_MISSMATCH_ERROR));
+  }
+
+  #[test]
+  fn test_when_list_expired_new_one_created() {
+    let mut s = Store::new();
+    let key = "foo";
+    let value = "bar";
+    let result = s.l_append(key.clone().to_string(), value.clone().to_string());
+
+    s.expire(key.clone().to_string(), Instant::now() + Duration::new(1, 0));
+    assert_eq!(result, Ok(()));
+
+    sleep(Duration::new(1, 0));
+
+    let new_value = "baz";
+    s.l_append(key.clone().to_string(), new_value.clone().to_string());
+
+    assert_eq!(s.l_get(key.clone().to_string(), &0), Some(&(new_value.to_string())));
+  }
+
+  #[test]
+  fn test_list_getall() {
+    let mut s = Store::new();
+    let key = "foo";
+    let value1 = "bar";
+    let value2 = "baz";
+
+    s.l_append(key.clone().to_string(), value1.clone().to_string());
+    s.l_append(key.clone().to_string(), value2.clone().to_string());
+
+    let result = s.l_getall(key.clone().to_string());
+
+    assert_eq!(result, Some(&vec!["bar".to_string(), "baz".to_string()]));
   }
 }
