799d5e45e4537c8f6465d80cb2b7eed658894409
Maxim Davydov <maximdavydoff@yandex.ru>
2017-09-29 17:37:45 +0300

    Updated reading function

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -11,7 +11,7 @@ use super::store::Store;
 struct Handler<R, W> {
   reader: R,
   writer: W,
-  buf: Vec<u8>
+  buf: Box<[u8]>
 }
 
 impl<R, W> Future for Handler<R, W>
@@ -23,12 +23,22 @@ impl<R, W> Future for Handler<R, W>
 
   fn poll(&mut self) -> Poll<u64, Error> {
     loop {
-      try_nb!(self.reader.read_to_end(&mut self.buf));
-      if self.buf == vec![1, 2, 3] {
+      let result = try_nb!(self.reader.read(&mut self.buf));
+      println!("{:?}", self.buf);
+      if self.buf[0] == 1 {
         return Ok(0.into())
       }
       self.buf[0] = b'>';
       try_nb!(self.writer.write(&self.buf));
+      self.flush(self.buf.len());
+    }
+  }
+}
+
+impl<R, W> Handler<R, W> {
+  fn flush(&mut self, amount: usize) {
+    for i in 0..amount {
+      self.buf[i] = 0
     }
   }
 }
@@ -58,7 +68,7 @@ impl Server {
       let mut h = Handler {
         reader,
         writer,
-        buf: Vec::new()
+        buf: Box::new([0; 2048])
       };
 
       let handle_conn = h.map(|v| {
